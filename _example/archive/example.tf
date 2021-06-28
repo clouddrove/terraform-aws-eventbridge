977941128b13275572d6b52e8bb90eaa442ffa40
HarmanJyot Kaur <harman@clouddrove.com>
2021-06-28 21:58:52 +0530

    added outputs in module

--- _example/archive/example.tf
+++ _example/archive/example.tf
@@ -2,25 +2,19 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_cloudwatch_event_bus" "existing_bus" {
-  name = "${random_pet.this.id}-existing-bus"
-}
-
-resource "random_pet" "this" {
-  length = 2
-}
-
-module "eventbridge_archive_only" {
+module "eventbridge_archive" {
   source = "../../"
 
-  create_bus      = false
+  create_bus      = true
   create_archives = true
+
+  bus_name = "clouddrove-bus"
+
 
   archives = {
     "launch-archive-existing-bus" = {
-      event_source_arn = aws_cloudwatch_event_bus.existing_bus.arn
-      description      = "${random_pet.this.id}-launch-archive",
-      retention_days   = 1
+      description    = "launch-archive",
+      retention_days = 1
       event_pattern = jsonencode(
         {
           "source" : ["aws.autoscaling"],
@@ -30,6 +24,5 @@ module "eventbridge_archive_only" {
     }
   }
 
-  depends_on = [aws_cloudwatch_event_bus.existing_bus]
 }
 
---
+++ output.tf
@@ -0,0 +1,31 @@
+# EventBridge Bus
+output "eventbridge_bus_name" {
+  description = "The EventBridge Bus Name"
+  value       = var.bus_name
+}
+
+output "eventbridge_bus_arn" {
+  description = "The EventBridge Bus Arn"
+  value       = element(concat(aws_cloudwatch_event_bus.this.*.arn, [""]), 0)
+}
+
+# EventBridge Archive
+output "eventbridge_archive_arns" {
+  description = "The EventBridge Archive Arns created"
+  value       = { for v in aws_cloudwatch_event_archive.this : v.name => v.arn }
+}
+
+# EventBridge Rule
+output "eventbridge_rule_ids" {
+  description = "The EventBridge Rule IDs created"
+  value = {
+    for p in sort(keys(var.rules)) : p => aws_cloudwatch_event_rule.this[p].id
+  }
+}
+
+output "eventbridge_rule_arns" {
+  description = "The EventBridge Rule ARNs created"
+  value = {
+    for p in sort(keys(var.rules)) : p => aws_cloudwatch_event_rule.this[p].arn
+  }
+}
